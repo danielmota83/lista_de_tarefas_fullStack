31c7119fddc9871500f88d620146162837cb7f89
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-09-01 22:35:34 -0300

    initial commit

---
+++ api/src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`API rodando em http://localhost:${PORT}`));
