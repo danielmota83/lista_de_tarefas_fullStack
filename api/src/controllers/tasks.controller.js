5fb5168e840a413d7e2c184b1c3c5c027da2ca94
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-09-02 12:19:54 -0300

    chore: building the api

---
+++ api/src/controllers/tasks.controller.js
@@ -0,0 +1,75 @@
+import tasksService from "../service/tasks.services.js";
+
+const allTasks = async (req, res) => {
+    const tasks = await tasksService.allTasks();
+  
+    if (tasks.length == 0) {
+      return res
+        .status(206)
+        .send({ message: 'Ainda não existe nenhuma tarefa cadastrada!' });
+    }
+  
+    res.send(tasks);
+  };
+
+  const taskById = async (req, res) => {
+    const id = req.params.id;
+  
+    const task = await tasksService.taskById(id);
+  
+    if (!task) {
+      res.status(206).send({ message: 'Nenhuma tarefa encontrada' });
+    } else {
+      res
+        .status(200)
+        .send({ message: 'Tarefa encontrada com sucesso', task });
+    }
+  };
+
+  const createTask = async (req, res) => {
+    const task = req.body;
+  
+    const newTask = await tasksService.createTask(task);
+  
+    res
+    .status(201)
+    .send({message: 'Sua tarefa foi criada com sucesso!', newTask});
+  };
+
+  const updateTaskById = async (req, res) => {
+    const id = req.params.id;
+    const taskEdit = req.body;
+  
+    const chosenTask = await tasksService.taskById(id);
+  
+    if (!chosenTask) {
+      return res.status(206).send({ message: 'Tarefa não encontrada!' });
+    }
+  
+    const updatedTask = await tasksService.updateTaskById(id, taskEdit);
+  
+    res.send(updatedTask);
+  };
+
+  const deleteTaskById = async (req, res) => {
+    const id = req.params.id;
+  
+    const chosenTask = await tasksService.taskById(id);
+  
+    if (!chosenTask) {
+      return res.status(206).send({ message: 'Nenhuma tarefa selecionada.' });
+    }
+  
+    await tasksService.deleteTask(id);
+  
+    return res.status(204).send({ message: 'Tarefa deletada!' });
+  };
+
+
+  export default {
+    allTasks,
+    taskById,
+    createTask,
+    updateTaskById,
+    deleteTaskById,
+}
